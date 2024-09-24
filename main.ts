603c87948900f2db4af5de14e4c73a5cad2e0296
kmmatin66 <65290307+kmmatin66@users.noreply.github.com>
2024-09-25 00:27:39 +0330

    goatb

--- main.ts
+++ main.ts
@@ -78,7 +78,7 @@ async function makeMoney(tokensAndNumbers: { token: string, number: number }[]):
 
 // لیستی از توکن‌ها و شماره‌های مرتبط با آن‌ها
 const tokensAndNumbers = [
-  { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZlZjQ0MmU1MDIzMDkwNjNkODkwNzJmIiwiaWF0IjoxNzI3MDkxNTc0LCJleHAiOjE3MjcxNzc5NzQsInR5cGUiOiJhY2Nlc3MifQ.Uj17uaarYLo6j1buP5FtoeHzBkuv5XYrtFTQFfmuemo", number: 9059549183 },
+  { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZlZjQ0MmU1MDIzMDkwNjNkODkwNzJmIiwiaWF0IjoxNzI3MjExNDEwLCJleHAiOjE3MjcyOTc4MTAsInR5cGUiOiJhY2Nlc3MifQ.EEKrQtXaQeY6weI6TmlSjE0nWrovhMKCINdSQ7c0jhk", number: 9059549183 },
     { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZlN2ZhYzI2M2Y3Mzg1MGY4YjJjYTRiIiwiaWF0IjoxNzI3MTc3MjIyLCJleHAiOjE3MjcyNjM2MjIsInR5cGUiOiJhY2Nlc3MifQ.WV49rBgvyaPBxE671lC_1m4LxditzzXiuDZqyP4FBu0", number: 9025967864 },
     { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZkZGZiNjYxZjdmMGYxNGZmYzkxNDAyIiwiaWF0IjoxNzI3MTc4NDgwLCJleHAiOjE3MjcyNjQ4ODAsInR5cGUiOiJhY2Nlc3MifQ.r0lrI87lv5CydivQOpWiDubB8fxfKSVC3reT4JJs23o", number: 9912984617 },
     { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZlN2Y2OThmZjM0YmE2YzMyYzM3ZGFmIiwiaWF0IjoxNzI3MTc4Nzg4LCJleHAiOjE3MjcyNjUxODgsInR5cGUiOiJhY2Nlc3MifQ.q47eq2T5JGCcctUVLfZoeKoXLJ6oYO6NrRuWuv3hg-4", number: 9964711498 },
